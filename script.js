0dfae317faa87b0e290958c4563e4e2f0d0958eb
isalazar16 <98783979+isalazar16@users.noreply.github.com>
2023-09-14 18:24:59 +0200

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('Hello!');
+
+<script>
+      window.onload = function () {
+        let irudia = document.getElementById('irudia');
+        irudia.onclick = function () {
+          console.log('irudia klikatu duzu');
+        };
+      };
+    </script>
